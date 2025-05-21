258e840ba850e2e03a52da4f256dd1c3633745b6
Delaksan-Sritharan <delaksan@gmail.com>
2025-05-22 00:51:31 +0530

    v.1

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 const generateButton = document.getElementById('generate-btn');
-const palatteContainer = document.getElementById('palette-container');
-
+const paletteContainer = document.querySelector('.palette-container');
 
 generateButton.addEventListener('click', generatePalette);
+
+
+paletteContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('copy-btn')) {
+        const hexValue = e.target.previousElementSibling.textContent;
+
+        navigator.clipboard.writeText(hexValue)
+            .then(() => showCopySuccess(e.target))
+            .catch(err => console.error(err))
+    } else if (e.target.classList.contains('color')) {
+        const hexValue = e.target.nextElementSibling.quarySelector('.hex-value').textContent;
+
+        navigator.clipboard.writeText(hexValue)
+            .then(() => showCopySuccess(e.target.nextElementSibling.quarySelector('.copy-btn')))
+            .catch(err => console.error(err))
+    }
+});
+
+function showCopySuccess(element) {
+    element.classList.remove("far", "fa-copy");
+    element.classList.add("fas", "fa-check");
+
+    element.style.color = "#48bb78";
+
+    setTimeout(() => {
+        element.classList.remove("fas", "fa-check");
+        element.classList.add("far", "fa-copy");
+        element.style.color = "";
+
+    }, 1500);
+
+}
 
 function generatePalette() {
     const colors = []
@@ -10,14 +41,31 @@ function generatePalette() {
     for (let i = 0; i < 5; i++) {
         colors.push(generateRandomColor());
     }
+    updatePaletteDisplay(colors)
 }
 
 function generateRandomColor() {
     const letters = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.floor(random() * 16))];
+        color += letters[Math.floor(Math.random() * 16)];
     }
+    return color
 }
 
-generatePalette();
+function updatePaletteDisplay(colors) {
+    const colorBoxes = document.querySelectorAll('.color-box');
+
+    colorBoxes.forEach((box, index) => {
+        const color = colors[index];
+        const colorDiv = box.querySelector('.color');
+        const hexValue = box.querySelector('.hex-value');
+
+        colorDiv.style.backgroundColor = color;
+        hexValue.textContent = color;
+    })
+}
+
+
+//If you want to generate the palette on page load, uncomment the line below
+// generatePalette();
